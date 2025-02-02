ef8dd8e25539b331201229d74b624159e06f287e
Maru <kotevode@icloud.com>
2025-02-02 22:14:35 +0700

    Revert "asd"
    This reverts commit b919848b602e692dc4b127cff56370f2c52ad83f.

--- tasksfile.js
+++ tasksfile.js
@@ -49,7 +49,10 @@ function buildProverServer() {
         " ../depends/ffiasm/c/naf.cpp"+
         " ../depends/ffiasm/c/splitparstr.cpp"+
         " ../depends/ffiasm/c/alt_bn128.cpp"+
-        " fr_asm.o"+
+        " fq.cpp"+
+        " fq.o"+
+        " fr.cpp"+
+        " fr.o"+
         " -L../depends/pistache/build/src -lpistache"+
         " -o proverServer"+
         " -fmax-errors=5 -pthread -std=c++17 -fopenmp -lgmp -lsodium -g -DSANITY_CHECK -DUSE_OPENMP -fpermissive", {cwd: "build", nopipe: true}
