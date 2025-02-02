a6898f07cd27f6b981cfc025d089994aad872bd4
Maru <kotevode@icloud.com>
2025-02-02 22:17:59 +0700

    asd

--- tasksfile.js
+++ tasksfile.js
@@ -50,9 +50,9 @@ function buildProverServer() {
         " ../depends/ffiasm/c/splitparstr.cpp"+
         " ../depends/ffiasm/c/alt_bn128.cpp"+
         " fq.cpp"+
-        " fq.o"+
+        // " libfq.o"+
         " fr.cpp"+
-        " fr.o"+
+        // " libfr.o"+
         " -L../depends/pistache/build/src -lpistache"+
         " -o proverServer"+
         " -fmax-errors=5 -pthread -std=c++17 -fopenmp -lgmp -lsodium -g -DSANITY_CHECK -DUSE_OPENMP -fpermissive", {cwd: "build", nopipe: true}
